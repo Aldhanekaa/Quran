a324e1c4992cedf79fd1f233b289bd7ec9e56a45
Aldhanekaa <aldhanekadev@gmail.com>
2021-03-24 08:36:22 +0700

    s

---
+++ next.config.js
@@ -0,0 +1,72 @@
+const withPWA = require("next-pwa");
+const withCss = require("@zeit/next-css");
+const withPurgeCss = require("next-purgecss");
+const withPlugins = require("next-compose-plugins");
+const optimizedImages = require("next-optimized-images");
+const {
+  konversiHari,
+  konversiBulan,
+  timeConversion
+} = require("./utils/konversiWaktu");
+
+const getDate = new Date();
+
+const NODE_ENV = process.env.NODE_ENV;
+const dualENV = {
+  production: {
+    PUBLIC_URL: "https://sekilas13.vercel.app"
+  },
+  development: {
+    PUBLIC_URL: "http://localhost:3000"
+  }
+};
+
+const env = { ...dualENV[NODE_ENV], isProduction: NODE_ENV === "production" };
+
+// next.js configuration
+const nextConfig = {
+  pageExtensions: [
+    "page.js",
+    "page.tsx",
+    "page.jsx",
+    "cPage.tsx",
+    "api.js",
+    "api.ts",
+    "_app.js",
+    "_document.js"
+  ],
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "If-Modified-Since",
+            value: `${konversiHari(
+              getDate.getDay()
+            )}, ${getDate.getDate()} ${konversiBulan(
+              getDate.getMonth()
+            )} ${getDate.getFullYear()} ${timeConversion(
+              getDate.getHours()
+            )}:${timeConversion(getDate.getMinutes())}:${timeConversion(
+              getDate.getSeconds()
+            )} GMT` // <day-name>, <day> <month> <year> <hour>:<minute>:<second> GMT | https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/If-Modified-Since
+          }
+        ]
+      }
+    ];
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      require("./utils/sitemap-robots-generator")(env.PUBLIC_URL);
+    }
+    config.module.rules.push({
+      test: /\.md$/,
+      use: "raw-loader"
+    });
+    return config;
+  },
+  env
+};
+
+module.exports = withPlugins([], nextConfig);

8dd849e350b0cca42471e61ae37c3c3798c3d98b
Aldhan (He/Him) <aldhanekadev@gmail.com>
2021-04-06 13:00:15 +0700

    Update _app.cPage.tsx

--- pages/_app.cPage.tsx
+++ pages/_app.cPage.tsx
@@ -134,7 +134,6 @@ function MyApp({ Component, pageProps }: AppProps) {
             {/* {Footer ? Footer : ""} */}
             <Footer />
 
-            <ScrollToTop router={router} />
           </ThemeProvider>
         </ChakraProvider>
       </Provider>

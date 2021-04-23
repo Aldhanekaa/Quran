c4e93f60030edfa318dd21596525d1f3eb0adbc9
Aldhan (He/Him) <aldhanekadev@gmail.com>
2021-04-23 20:56:51 +0700

    search chapter API

---
+++ pages/api/chapter/[chapter].api.ts
@@ -0,0 +1,49 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import {
+  ErrorMessage,
+  surahInfoType,
+  chapter,
+  Verse,
+  chapters
+} from "@/ts/interfaces";
+
+import axios from "axios";
+
+// @ts-ignore
+interface verseAPI extends NextApiRequest {
+  query: {
+    chapter: string;
+    showVerseFetch?: boolean;
+  };
+}
+
+interface VerseFetchResult {
+  verse: Verse;
+}
+
+export default async function getVerseById(
+  req: verseAPI,
+  res: NextApiResponse
+) {
+  console.log(req.query.chapter);
+  const chapter = req.query.chapter.toLowerCase();
+
+  //@ts-ignore
+  const {
+    data: { chapters }
+  } = await axios.get<chapters>(
+    `https://api.quran.com/api/v4/chapters?language=en`
+  );
+
+  let found: chapter[] = [];
+  console.log(chapters);
+
+  for (let i = 0; i < chapters.length; i++) {
+    console.log(chapters[i].name_simple);
+    if (chapters[i].name_simple.toLowerCase().indexOf(chapter) > -1) {
+      found.push(chapters[i]);
+    }
+  }
+
+  res.json({ chapters: found });
+}
